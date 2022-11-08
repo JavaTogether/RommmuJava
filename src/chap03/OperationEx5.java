0c9dbb809091d55eb88f53af73756da6d5053e01
Rommmu <tonty1222@naver.com>
2022-11-08 18:12:57 +0900

    chap02 여러가지 연산자

---
+++ src/chap03/OperationEx5.java
@@ -0,0 +1,21 @@
+package chap03;
+
+public class OperationEx5 {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+
+        int result1 = num1 & num2;
+        System.out.println(result1);
+
+        int result2 = num1 | num2;
+        System.out.println(result2);
+
+        int result3 = num1 ^ num2;
+        System.out.println(result3);
+
+        int result4 = ~num1;
+        System.out.println(result4);
+
+    }
+}
---
+++ src/chap03/OperationEx6.java
@@ -0,0 +1,9 @@
+package chap03;
+
+public class OperationEx6 {
+    public static void main(String[] args) {
+        int num1 = 5;
+        System.out.println(num1 <<= 1);
+        System.out.println(num1);
+    }
+}
