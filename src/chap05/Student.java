ff9847867633bcea69df86e469af6c1595bf5cd0
Rommmu <tonty1222@naver.com>
2022-11-15 13:53:53 +0900

    chap05 클래스와 객체 02

--- src/chap05/Student.java
+++ src/chap05/Student.java
@@ -6,16 +6,41 @@ public class Student {
     int grade;
     String address;
 
+    public Student() {
+
+    }
+    public Student(int id, String name) {
+        studentId = id;
+        studentName = name;
+    }
+
     public void showStudentInfo() {
         System.out.println(studentName + ", " + address);
     }
 
-    /*public static void main(String[] args) {
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String name) {
+        studentName = name;
+    }
+
+    public static void main(String[] args) {
         Student studentLee = new Student();
         studentLee.studentName = "이순신";
-        studentLee.address = "경기도 고양시";
-
+        studentLee.studentId = 100;
+        studentLee.address = "여의도";
         studentLee.showStudentInfo();
-    }*/
+
+        Student studentJoo = new Student();
+        studentJoo.studentName = "주영빈";
+        studentJoo.studentId = 101;
+        studentJoo.address = "경기도 고양시";
+        studentJoo.showStudentInfo();
+
+        System.out.println(studentLee);
+        System.out.println(studentJoo);
+    }
 }
 
