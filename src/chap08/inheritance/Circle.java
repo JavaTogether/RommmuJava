746c9a91430597e5f0b9ed4848ace50b76ebb660
Rommmu <tonty1222@naver.com>
2023-01-07 17:38:12 +0900

    상속과 다형성

---
+++ src/chap08/inheritance/Circle.java
@@ -0,0 +1,11 @@
+package chap08.inheritance;
+
+public class Circle {
+    Point point; // 가져오는 것 has - a 관계
+    private int radius;
+
+    public Circle() {
+        point = new Point();
+    }
+
+}
---
+++ src/chap08/polymorphism/AnimalTest.java
@@ -0,0 +1,38 @@
+package chap08.polymorphism;
+
+class Animal {
+    public void move() {
+        System.out.println("동물이 움직입니다.");
+    }
+}
+
+class Human extends Animal {
+    public void move() {
+        System.out.println("사람이 두 발로 걷습니다.");
+    }
+}
+
+class Tiger extends Animal {
+    public void move() {
+        System.out.println("호랑이가 네 발로 움직입니다.");
+    }
+}
+
+class Eagle extends Animal {
+    public void move() {
+        System.out.println("독수리가 하늘을 납니다.");
+    }
+}
+
+public class AnimalTest {
+    public static void main(String[] args) {
+        AnimalTest test = new AnimalTest();
+        test.moveAnimal(new Human());
+        test.moveAnimal(new Tiger());
+        test.moveAnimal(new Eagle());
+    }
+
+    public void moveAnimal(Animal animal) {
+        animal.move();
+    }
+}
